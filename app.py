50a8467d94d242e83b54616a4691a4897683388a
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2023-10-07 13:13:25 +0545

    added training part

---
+++ app.py
@@ -0,0 +1,30 @@
+import torch
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+def generate_response(model, tokenizer, prompt, max_length=100, temperature=0.1, k=50):
+    input_ids = tokenizer.encode(prompt, return_tensors="pt")
+    
+    attention_mask = torch.ones_like(input_ids)
+    pad_token_id = tokenizer.eos_token_id
+
+    output = model.generate(
+        input_ids,
+        max_length=max_length,
+        num_return_sequences=1,
+        attention_mask=attention_mask,
+        pad_token_id=pad_token_id,
+        temperature=temperature,
+        top_k=k
+    )
+
+    return tokenizer.decode(output[0], skip_special_tokens=True)
+
+
+model_path = "./model"
+my_chat_model = GPT2LMHeadModel.from_pretrained(model_path)
+my_chat_tokenizer = GPT2Tokenizer.from_pretrained(model_path)
+
+prompt = input()
+print("Bot is thinking")
+response = generate_response(my_chat_model, my_chat_tokenizer, prompt, max_length=100)
+print("Generated response:", response)
