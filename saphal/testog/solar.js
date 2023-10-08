a18a7ff2374fa8d1fa338d19a66e2a31394bf70e
saphal-upreti <108993335+saphal-upreti@users.noreply.github.com>
2023-10-08 10:28:09 +0545

    orbitchanges

--- saphal/testog/solar.js
+++ saphal/testog/solar.js
@@ -32,58 +32,58 @@ try {
   console.log("error");
 }
 
-const sun = document.getElementById("sun");
-const mer = document.getElementById("mercury");
-const ven = document.getElementById("venus");
-const ear = document.getElementById("earth");
-const mar = document.getElementById("mars");
-const jup = document.getElementById("jupiter");
-const satn = document.getElementById("saturn");
-const urns = document.getElementById("uranus");
-const nep = document.getElementById("neptune");
+// const sun = document.getElementById("sun");
+// const mer = document.getElementById("mercury");
+// const ven = document.getElementById("venus");
+// const ear = document.getElementById("earth");
+// const mar = document.getElementById("mars");
+// const jup = document.getElementById("jupiter");
+// const satn = document.getElementById("saturn");
+// const urns = document.getElementById("uranus");
+// const nep = document.getElementById("neptune");
 
-const sunLeft = parseInt(sun.getBoundingClientRect().left);
-const sunWidth = parseInt(sun.offsetWidth);
-const sunTop = parseInt(sun.getBoundingClientRect().top);
-const sunHeight = parseInt(sun.offsetHeight);
-var h = parseInt(sunLeft + sunWidth / 2);
-var k = parseInt(sunTop + sunHeight / 2);
+// const sunLeft = parseInt(sun.getBoundingClientRect().left);
+// const sunWidth = parseInt(sun.offsetWidth);
+// const sunTop = parseInt(sun.getBoundingClientRect().top);
+// const sunHeight = parseInt(sun.offsetHeight);
+// var h = parseInt(sunLeft + sunWidth / 2);
+// var k = parseInt(sunTop + sunHeight / 2);
 // console.log(x,y)
 
-var xValue = (planetName) => {
-  var r = parseInt(planetName.getBoundingClientRect().left) - h;
-  return x;
-};
-var rValue = (planetName) => {
-  var r = parseInt(planetName.getBoundingClientRect().left) - h;
-  return r;
-};
-if (document.documentElement.scrollWidth > 5000) {
-  var x = parseInt(xValue(jup))
-  var r = parseInt(rValue(jup))
-}
+// var xValue = (planetName) => {
+//   var r = parseInt(planetName.getBoundingClientRect().left) - h;
+//   return x;
+// };
+// var rValue = (planetName) => {
+//   var r = parseInt(planetName.getBoundingClientRect().left) - h;
+//   return r;
+// };
+// if (document.documentElement.scrollWidth > 5000) {
+//   var x = parseInt(xValue(jup))
+//   var r = parseInt(rValue(jup))
+// }
 
-const planetLoop = () => {
-  let dum1 = Math.pow(x - h, 2);
-  let dum2 = Math.pow(r, 2);
-  let dum3 = Math.sqrt(dum2 - dum1);
+// const planetLoop = () => {
+//   let dum1 = Math.pow(x - h, 2);
+//   let dum2 = Math.pow(r, 2);
+//   let dum3 = Math.sqrt(dum2 - dum1);
 
-  var y = dum3 + k;
-  // planetName.style.left = x + "px";
-  // planetName.style.top = y + "px";
-  // // console.log('hi')
-  // console.log(x);
-  x = x - 1;
-  if (x == h - r) {
-    clearInterval(planet(jup));
-  }
-  console.log(x, y);
-};
+//   var y = dum3 + k;
+//   planetName.style.left = x + "px";
+//   planetName.style.top = y + "px";
+//   console.log('hi')
+//   console.log(x);
+//   x = x - 1;
+//   if (x == h - r) {
+//     clearInterval(planet(jup));
+//   }
+//   console.log(x, y);
+// };
 
-const jupiter = () => {
-  planetLoop(x);
-};
-if (document.documentElement.scrollWidth > 5000) {
-  console.log("hi");
-  setInterval(jupiter, 1000);
-}
+// const jupiter = () => {
+//   planetLoop(x);
+// };
+// if (document.documentElement.scrollWidth > 5000) {
+//   console.log("hi");
+//   setInterval(jupiter, 1000);
+// }
